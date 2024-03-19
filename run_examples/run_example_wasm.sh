e6dcb225d2679e84691bea120abf4e67fae8cc88
0rphon <59403052+0rphon@users.noreply.github.com>
2024-03-19 20:04:39 +0000

    cleanup scripts

--- run_examples/run_example_wasm.sh
+++ run_examples/run_example_wasm.sh
@@ -5,7 +5,5 @@ cd "$here/.."
 cargo build --target wasm32-unknown-unknown --release --example mini_proxy
 cd "$here"
 cp ../target/wasm32-unknown-unknown/release/examples/mini_proxy.wasm config/mini_proxy.wasm
-docker-compose -f docker-compose-wasm.yaml down -v
-docker-compose -f docker-compose-wasm.yaml build --no-cache
 docker-compose -f docker-compose-wasm.yaml up
 rm config/mini_proxy.wasm
